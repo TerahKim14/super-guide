b0da7ce9631673758c00f4f7241550f739523f6e
TerahKim14 <terahdogozari@gmail.com>
2025-05-25 01:40:55 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/Header';
+import MainComponent from './components/MainComponent';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainComponent />
+    </div>
+  );
+}
+
+export default App;
